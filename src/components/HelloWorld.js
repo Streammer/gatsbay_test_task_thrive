169a9faf79e8a0e1918d238829d3f0ab6154f6bb
dzmitry.lukashenka <Lukdmitrij@yandex.ru>
2021-07-22 11:36:20 +0300

    base components

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import Link from "gatsby";
+
+const HelloWorld = () => {
+    return (
+        <>
+            <h1>
+                <Link to=https://www.google.com">
+                    Hello World
+                </Link>
+            </h1>
+        </>
+    );
+};
+
+export default HelloWorld;
