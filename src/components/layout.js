b049808df23f0d34c8b2148abaae4fbae7df4ade
dzmitry.lukashenka <Lukdmitrij@yandex.ru>
2021-07-22 16:07:33 +0300

    refactoring

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,12 +8,12 @@
 import * as React from 'react'
 import HelloWorld from './HelloWorld/HelloWorld'
 import Video from './Video/Video'
-import {videoSrcURL} from "../constants/statikUrls"
 import './layout.css'
 
 const Layout = () => {
     const videoTitle = 'YouTube video player'
     const greeting = 'Hello World'
+    const videoSrcURL = 'https://www.youtube.com/embed/kr4g2h5YmnU'
     return (
         <>
             <HelloWorld greeting={greeting}/>
--- src/constants/statikUrls.js
+++ src/constants/statikUrls.js
@@ -1,2 +1,1 @@
 export const linkToGoogle = 'https://www.google.com'
-export const videoSrcURL = 'https://www.youtube.com/embed/kr4g2h5YmnU'
